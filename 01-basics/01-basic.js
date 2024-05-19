53de3deb2893419d54fe76e79a30e16402d61a8e
Muzamil...Khan <146024324+mk2249@users.noreply.github.com>
2024-05-19 19:33:53 +0000

    day1

---
+++ 01-basics/01-basic.js
@@ -0,0 +1,5 @@
+const accountId = 1234;
+let accountEmail = "muzail01@gmail.com";
+var accountPassword = "12345";
+accountCity = "Lahore";
+console.table([accountId,accountEmail,accountPassword,accountCity])
